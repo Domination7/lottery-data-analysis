e93d68bf5b6ba0d21300b91c168321ef5e885596
Domination7 <31801630+Domination7@users.noreply.github.com>
2018-01-17 14:01:52 -0600

    Added better reading for new lotteries

--- lottery.py
+++ lottery.py
@@ -1,7 +1,10 @@
 import csv
 
-
+#   The indexed lottery matrices
 megaMillionTable = []
+powerballTable = []
+currentLottery = []
+
 #   These are all the column numbers
 date = 0
 numOne = 1
@@ -16,34 +19,47 @@ megaBallNum = 12
 megaBallCo = 13
 
 #   To read a value, the format is lotteryTable[row][column]
-
 with open('MegaMillionsData.csv', newline='') as file:
     reader = csv.reader(file)
     for row in reader:
         megaMillionTable.append(row)
 
+
 #   Creates the table of all indexed lottery games
 lotteryTable = []
 lotteryTable.append("Mega Millions")
+lotteryTable.append("Powerball")
 
 
 #   Choosing which lottery to view info of
 counter = 0
 for row in lotteryTable:
     print("{",counter,"}\t",row)
+    counter+=1
     
-question = input("\nWhich lottery would you like to see? ")
+question = int(input("\nWhich lottery would you like to see? "))
 
+#   This is the current lottery chosen by the user
+if(question == 0):
+    currentLottery = megaMillionTable
+
+if(question == 1):
+    currentLottery = powerballTable
+   
+    
+#   Question of if you want to find a number
 question = input("Would you like to find a number? Y or N ")
 if question == 'Y':
     rowValue = int(input("Row Number? "))
     colValue = int(input("Column Number? "))
-    print(megaMillionTable[rowValue][colValue])
+    print(currentLottery[rowValue][colValue])
 
+
+#   Viewing the numbers on a given day
 counter = 0
 question = input("Would you like to see the numbers on a given day? Y or N ")
 if question == 'Y':
-    for i in megaMillionTable:
+    for i in currentLottery:
         if counter < 1:
             print("\n--- ",i[0]," ---")
             counter+=1
